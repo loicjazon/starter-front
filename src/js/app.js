1b48884ff910dadfc8549ca1bbb113bb6d57aa05
Loïc Jazon <contact@loicjazon.com>
2017-05-07 11:58:31 +0200

    bootstrap style and multiple carousels

--- src/js/app.js
+++ src/js/app.js
@@ -7,22 +7,28 @@ $(function() {
   }
 
   let Carousel = {
+
+    elements: $('.js-carousel'),
+
     init: function () {
       this.singleItem()
       this.mutipleItems()
     },
+
     singleItem: function () {
       console.log("lancement des carousels simples")
-      $('.carousel-single-item').slick()
+      this.elements.filter('.js-carousel-single-item').slick()
     },
+
     mutipleItems: function () {
       console.log("lancement des carousels multiple")
-      $('.carousel-multiple-items').slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3
+      this.elements.filter('.js-carousel-multiple-items').slick({
+         infinite: true,
+         slidesToShow: 3,
+         slidesToScroll: 3
       });
     }
+    
   }
 
   Menu.init()
